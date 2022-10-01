71f0ff42daf02df747cb4e19a904578d9ba9e7da
FadhilAF <falfatih3421@gmail.com>
2022-10-01 17:44:58 +0700

    add status 400 for error handler

--- src/middlewares/errorHandler.ts
+++ src/middlewares/errorHandler.ts
@@ -3,7 +3,8 @@ import { ErrorRequestHandler } from "express";
 const errorHandler: ErrorRequestHandler = (err, req, res, next) => {
   console.log(err.message)
 
-  res.json({ error: Object.entries(err).length == 0 ? err.message : err });
+  //jangan lupo kasi kode http error
+  res.status(400).json({ error: Object.entries(err).length == 0 ? err.message : err });
 };
 
 export default errorHandler;
